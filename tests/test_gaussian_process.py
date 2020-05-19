28cbb75e3d473cb601ff5608345ecc5329d94bdf
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-19 16:24:28 +0900

    Add test case for gp

---
+++ tests/test_gaussian_process.py
@@ -0,0 +1,32 @@
+
+import unittest
+
+import torch
+
+import neuralprocess as npr
+
+
+class TestGaussianProcess(unittest.TestCase):
+
+    def setUp(self):
+        self.y_dim = 2
+        self.model = npr.GaussianProcess(y_dim=self.y_dim)
+
+    def test_gaussian_kernel(self):
+        batch_size = 5
+        num_points = 10
+        x_dim = 3
+        x = torch.randn(batch_size, num_points, x_dim)
+
+        kernel = self.model.gaussian_kernel(x)
+        self.assertTupleEqual(
+            kernel.size(), (batch_size, self.y_dim, num_points, num_points))
+
+    def test_inference(self):
+        batch_size = 5
+        num_points = 10
+        x_dim = 3
+        x = torch.randn(batch_size, num_points, x_dim)
+        y = self.model.inference(x)
+
+        self.assertTupleEqual(y.size(), (batch_size, num_points, self.y_dim))
