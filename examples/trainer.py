Fix variance in cnp

examples/trainer.py
@@ -211,7 +211,7 @@ class Trainer:
         # Query y_target
         x_ctx, y_ctx, x_tgt, y_tgt = next(iter(self.test_loader))
         with torch.no_grad():
-            mu, logvar = self.model.query(x_ctx, y_ctx, x_tgt)
+            mu, var = self.model.query(x_ctx, y_ctx, x_tgt)
 
         # Plot
         plt.figure(figsize=(10, 6))
@@ -222,8 +222,8 @@ class Trainer:
         plt.plot(x_tgt.squeeze(-1)[0], mu.squeeze(-1)[0], "b",
                  label="Sampled function")
         plt.fill_between(x_tgt.squeeze(-1)[0],
-                         (mu + torch.exp(0.5 * logvar)).squeeze(-1)[0],
-                         (mu - torch.exp(0.5 * logvar)).squeeze(-1)[0],
+                         (mu + var ** 0.5).squeeze(-1)[0],
+                         (mu - var ** 0.5).squeeze(-1)[0],
                          color="b", alpha=0.2, label="1-sigma range")
         plt.legend(loc="upper right")
         plt.title(f"Training results (epoch={epoch})")

neuralprocess/base_np.py
@@ -47,7 +47,7 @@ class BaseNP(nn.Module):
         Returns:
             mu (torch.Tensor): y queried by target x and encoded
                 representation, size `(batch_size, num_target, y_dim)`.
-            logvar (torch.Tensor): Log variance of y, size
+            var (torch.Tensor): Variance of y, size
                 `(batch_size, num_target, y_dim)`.
         """
 

neuralprocess/conditional_np.py
@@ -83,7 +83,7 @@ class Decoder(nn.Module):
         )
 
         self.fc_mu = nn.Linear(128, y_dim)
-        self.fc_logvar = nn.Linear(128, y_dim)
+        self.fc_var = nn.Linear(128, y_dim)
 
     def forward(self, x: Tensor, r: Tensor) -> Tuple[Tensor, Tensor]:
         """Forward method.
@@ -97,7 +97,7 @@ class Decoder(nn.Module):
         Returns:
            mu (torch.Tensor): Decoded mean, size
                 `(batch_size, num_points, x_dim)`.
-           logvar (torch.Tensor): Decoded log variance, size
+           var (torch.Tensor): Decoded variance, size
                 `(batch_size, num_points, x_dim)`.
         """
 
@@ -114,13 +114,13 @@ class Decoder(nn.Module):
         # Forward
         h = self.fc(h)
         mu = self.fc_mu(h)
-        logvar = F.softplus(self.fc_logvar(h))
+        var = F.softplus(self.fc_var(h))
 
         # Bring back into original shape
         mu = mu.reshape(batch_size, num_points, -1)
-        logvar = logvar.reshape(batch_size, num_points, -1)
+        var = var.reshape(batch_size, num_points, -1)
 
-        return mu, logvar
+        return mu, var
 
 
 class ConditionalNP(BaseNP):
@@ -158,13 +158,13 @@ class ConditionalNP(BaseNP):
         Returns:
             mu (torch.Tensor): y queried by target x and encoded
                 representation, size `(batch_size, num_target, y_dim)`.
-            logvar (torch.Tensor): Log variance of y, size
+            var (torch.Tensor): Variance of y, size
                 `(batch_size, num_target, y_dim)`.
         """
 
         representation = self.encoder(x_context, y_context)
-        mu, logvar = self.decoder(x_target, representation)
-        return mu, logvar
+        mu, var = self.decoder(x_target, representation)
+        return mu, var
 
     def loss_func(self, x_context: Tensor, y_context: Tensor, x_target: Tensor,
                   y_target: Tensor) -> Dict[str, Tensor]:
@@ -184,12 +184,12 @@ class ConditionalNP(BaseNP):
             loss_dict (dict of [str, torch.Tensor]): Calculated loss.
         """
 
-        mu, logvar = self.query(x_context, y_context, x_target)
+        mu, var = self.query(x_context, y_context, x_target)
 
         # Distribution
-        batch_size, num_target, y_dim = logvar.size()
+        batch_size, num_target, y_dim = var.size()
         cov = (torch.eye(y_dim).repeat(batch_size, num_target, 1, 1)
-               * torch.exp(logvar).unsqueeze(-1))
+               * var.unsqueeze(-1))
         dist = MultivariateNormal(mu, cov)
         log_p = dist.log_prob(y_target).sum()
 

tests/test_conditional_np.py
@@ -24,11 +24,11 @@ class TestEncoder(unittest.TestCase):
         x_target = torch.randn(batch_size, num_target, self.x_dim)
 
         # Forward
-        mu, logvar = self.model.query(x_context, y_context, x_target)
+        mu, var = self.model.query(x_context, y_context, x_target)
 
         self.assertTupleEqual(mu.size(), (batch_size, num_target, self.y_dim))
         self.assertTupleEqual(
-            logvar.size(), (batch_size, num_target, self.y_dim))
+            var.size(), (batch_size, num_target, self.y_dim))
 
     def test_loss_func(self):
         # Data