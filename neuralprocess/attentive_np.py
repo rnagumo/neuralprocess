Fix in-place computation

neuralprocess/attentive_np.py
@@ -115,7 +115,7 @@ class StochasticEncoder(nn.Module):
         var = F.softplus(self.fc_var(s))
 
         # Bounds variance > 0.01 (original code: sigma > 0.1)
-        var += 0.01
+        var = var + 0.01
 
         return mu, var
 
@@ -174,7 +174,7 @@ class Decoder(nn.Module):
         var = F.softplus(self.fc_var(h))
 
         # Bounds variance > 0.01 (original code: sigma > 0.1)
-        var += 0.01
+        var = var + 0.01
 
         return mu, var
 

neuralprocess/neural_process.py
@@ -62,7 +62,7 @@ class Encoder(nn.Module):
         var = F.softplus(self.fc_var(r))
 
         # Bounds variance > 0.01 (original code: sigma > 0.1)
-        var += 0.01
+        var = var + 0.01
 
         return mu, var
 
@@ -117,7 +117,7 @@ class Decoder(nn.Module):
         var = F.softplus(self.fc_var(h))
 
         # Bounds variance > 0.01 (original code: sigma > 0.1)
-        var += 0.01
+        var = var + 0.01
 
         return mu, var
 