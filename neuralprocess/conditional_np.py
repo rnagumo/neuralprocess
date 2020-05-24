Fix variance bounds in cnp

neuralprocess/conditional_np.py
@@ -106,7 +106,7 @@ class Decoder(nn.Module):
         var = F.softplus(self.fc_var(h))
 
         # Bounds variance > 0.01 (original code: sigma > 0.1)
-        var = 0.01 + 0.99 * var
+        var = 0.01 + var
 
         return mu, var
 